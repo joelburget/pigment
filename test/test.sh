1f873b2e81d13522332dfc7d9f5d60f352b6525e
adam.gundry@cis.strath.ac.uk <unknown>
2010-01-15 11:37:55 +0000

    Test script shows diff on failure

--- test/test.sh
+++ test/test.sh
@@ -26,9 +26,10 @@ do
     then
 	echo -e "\e[00;34m[UNDEFINED]\e[00m Please provide the desired output for $script"
     else
-	if ! diff ".tests/$script.log" "results/$script.log" > /dev/null 
+	if ! diff -u "results/$script.log" ".tests/$script.log" > ".tests/$script.diff"
 	then
 	    echo -e "\e[00;31m[FAILED]\e[00m $script does not match the expected output"
+	    cat ".tests/$script.diff"
 	else
 	    echo -e "\e[00;32m[PASSED]\e[00m $script looks alright!"
 	fi
