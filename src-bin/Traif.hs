e193697e2e9c2e5936fc81abe5ece4cb60ecada4
Joel Burget <joelburget@gmail.com>
2015-06-30 20:41:53 +0800

    Add another script to Traif.

--- src-bin/Traif.hs
+++ src-bin/Traif.hs
@@ -1,7 +1,7 @@
-{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE PatternSynonyms, ScopedTypeVariables #-}
 module Traif (main) where
 
-import Control.Applicative (many)
+import Control.Applicative (many, optional)
 import Control.Arrow (left)
 import Control.Exception
 import Control.Monad.State
@@ -23,10 +23,14 @@ import ProofState.Edition.GetSet
 import ProofState.Edition.Navigation
 import ProofState.Edition.ProofContext
 import ProofState.Edition.ProofState
+import ProofState.Interface.Module
 import ProofState.Interface.ProofKit
+import ProofState.Interface.Search
+import ProofState.Interface.Solving
 import ProofState.Structure.Developments
 import ProofState.Structure.Entries
 import Tactics.Data
+import Tactics.ProblemSimplify
 
 -- elabLet :: (String :<: Scheme DInTmRN) -> ProofState (EXTM :=>: VAL)
 -- elabProgram :: [String] -> ProofState (EXTM :=>: VAL)
@@ -37,6 +41,22 @@ import Tactics.Data
 
 script :: ProofState ()
 script = do
+    makeModule DevelopModule "Math"
+    goIn
+    makeModule DevelopModule "Algebra"
+    goIn
+
+    -- let scheme = SchExplicitPi ("x" :<: SchType DSET) (SchType DSET)
+    let scheme = SchType DSET
+    elabLet ("ring" :<: scheme)
+    optional problemSimplify
+    _ <- many goOut
+    return ()
+
+
+script'' :: ProofState ()
+script'' = do
+
     _ <- elabData "Foo" [] []
     _ <- many goOut
     return ()
@@ -68,25 +88,27 @@ script' = do
     _ <- many goOut
     return ()
 
-main :: IO ()
-main = do
+main' :: Either String (String, ProofContext)
+main' =
     let runner :: ProofState String
         runner = script >> prettyProofState
-        val :: String
-        val = case runTraifProofState runner emptyContext of
+    in runTraifProofState runner emptyContext
+
+main :: IO ()
+main = do
+    let val = case main' of
             Left err -> err
-            Right (view, _) -> view
+            Right (view, ctx) -> view ++ "\n\n" ++ show ctx
+    putStrLn val `catch` \ (ex :: PiTyException) -> showPity ex
 
-    putStrLn val `catch` \ (ex :: PityException) -> showPity ex
-
-showPity :: PityException -> IO ()
-showPity (PityException name ty) = do
+showPity :: PiTyException -> IO ()
+showPity (PiTyException name ty) = do
     putStrLn "Caught pity error:"
     putStrLn $ "name: " ++ name
     let val = case runTraifProofState (prettyHere (SET :>: ty)) emptyContext of
                   Left err -> err
                   Right (doc, _) -> renderHouseStyle doc
-    putStrLn val
+    putStrLn $ "type: " ++ val
     putStrLn "\n"
 
 -- Given a proof state command and a context, we can run the command with
@@ -99,85 +121,3 @@ runTraifProofState
 runTraifProofState m loc =
     let result = runStateT (m `catchStack` catchUnprettyErrors) loc
     in left show result
-
-prettyProofState :: ProofState String
-prettyProofState = do
-    inScope <- getInScope
-    me <- getCurrentName
-    d <- prettyPS inScope me
-    return (renderHouseStyle d)
-
-prettyPS :: Entries -> Name -> ProofState Doc
-prettyPS aus me = do
-        es <- replaceEntriesAbove B0
-        cs <- putBelowCursor F0
-        case (es, cs) of
-            (B0, F0)  -> prettyEmptyTip
-            _   -> do
-                d <- prettyEs empty (es <>> F0)
-                d' <- case cs of
-                    F0  -> return d
-                    _   -> do
-                        d'' <- prettyEs empty cs
-                        return (d $$ text "---" $$ d'')
-                tip <- prettyTip
-                putEntriesAbove es
-                _ <- putBelowCursor cs
-                return (lbrack <+> d' $$ rbrack <+> tip)
- where
-    prettyEs :: Doc -> Fwd (Entry Bwd) -> ProofState Doc
-    prettyEs d F0         = return d
-    prettyEs d (e :> es) = do
-        putEntryAbove e
-        ed <- prettyE e
-        prettyEs (d $$ ed) es
-
-    prettyE (EPARAM (_ := DECL :<: ty) (x, _) k _ anchor _)  = do
-        ty' <- bquoteHere ty
-        tyd <- prettyHereAt (pred ArrSize) (SET :>: ty')
-        return (prettyBKind k
-                 (text x  <+> (brackets $ brackets $ text $ show anchor)
-                          <+> kword KwAsc
-                          <+> tyd))
-
-    prettyE e = do
-        goIn
-        d <- prettyPS aus me
-        goOut
-        return (sep  [  text (fst (entryLastName e))
-                        <+> (brackets $ brackets $ text $ show $ entryAnchor e)
-                     ,  nest 2 d <+> kword KwSemi
-                     ])
-
-    prettyEmptyTip :: ProofState Doc
-    prettyEmptyTip = do
-        tip <- getDevTip
-        case tip of
-            Module -> return (brackets empty)
-            _ -> do
-                tip <- prettyTip
-                return (kword KwDefn <+> tip)
-
-    prettyTip :: ProofState Doc
-    prettyTip = do
-        tip <- getDevTip
-        case tip of
-            Module -> return empty
-            Unknown (ty :=>: _) -> do
-                hk <- getHoleKind
-                tyd <- prettyHere (SET :>: ty)
-                return (prettyHKind hk <+> kword KwAsc <+> tyd)
-            Suspended (ty :=>: _) prob -> do
-                hk <- getHoleKind
-                tyd <- prettyHere (SET :>: ty)
-                return (text ("(SUSPENDED: " ++ show prob ++ ")")
-                            <+> prettyHKind hk <+> kword KwAsc <+> tyd)
-            Defined tm (ty :=>: tyv) -> do
-                tyd <- prettyHere (SET :>: ty)
-                tmd <- prettyHereAt (pred ArrSize) (tyv :>: tm)
-                return (tmd <+> kword KwAsc <+> tyd)
-
-    prettyHKind :: HKind -> Doc
-    prettyHKind Waiting     = text "?"
-    prettyHKind Hoping      = text "HOPE?"
-    prettyHKind (Crying s)  = text ("CRY <<" ++ s ++ ">>")
