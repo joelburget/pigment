157c4b0b053d3ce9257fe0190524f23b5c5f5e14
Joel Burget <joelburget@gmail.com>
2015-07-06 08:23:32 +0800

    Style page layout, include messages.

--- src-web/js/view/PageLayout.js
+++ src-web/js/view/PageLayout.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import View from 'react-flexbox'
+import {FontIcon, Styles} from 'material-ui';
+import Radium from 'radium';
 
+let ThemeManager = new Styles.ThemeManager();
+
+let styles = {
+  page: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  commandLine: {
+    flex: '1'
+  }
+};
+
+@Radium
 export default class PageLayout extends React.Component {
   render() {
-    let [editView, commandLine] = this.props.children;
+    let [editView, commandLine, messages] = this.props.children;
 
-    return <View column>
-      <View>{editView}</View>
-      <View>{commandLine}</View>
-    </View>;
+    return <div style={styles.page}>
+      <div>{editView}</div>
+      <div styles={[styles.commandLine]}>{commandLine}</div>
+      <div>{messages}</div>
+    </div>;
   }
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
+  }
+
 }
+
+PageLayout.childContextTypes = {
+  muiTheme: React.PropTypes.object
+};
