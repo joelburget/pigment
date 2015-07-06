2a611f8a23ab593a5000538055f0854db08ff052
Joel Burget <joelburget@gmail.com>
2015-07-06 08:17:47 +0800

    Set up react-hot-loader.
    I still need to fix the problem of giving it access to the React
    instance.

--- src-web/js/webpack.config.js
+++ src-web/js/webpack.config.js
@@ -1,7 +1,15 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: "./exports",
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server',
+    './exports',
+  ],
+
   output: {
     path: __dirname + "/../../build/",
+    publicPath: '/assets/',
     filename: "jsview.js",
     libraryTarget: "var",
     library: "PigmentView"
@@ -12,13 +20,15 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          stage: 0
-        }
+        loaders: ['react-hot', 'babel-loader?stage=0'],
       },
     ]
   },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
 
   externals: {
     react: "React"
