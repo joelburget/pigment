0cd178279922c3d1ac61fd1721e6333bb262423e
Joel Burget <joelburget@gmail.com>
2015-07-14 15:12:07 +0800

    Messages point check.

--- src-web/js/view/Messages.js
+++ src-web/js/view/Messages.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import Radium from 'radium';
+import * as MaterialUI from 'material-ui';
+
+let { Styles: { Colors } } = MaterialUI;
+
+var styles = {
+  green: { color: Colors.green500 },
+  orange: { color: Colors.orange500 },
+  red: { color: Colors.red500 },
+}
 
 @Radium
 export class MessagesLayout extends React.Component {
@@ -7,5 +16,35 @@ export class MessagesLayout extends React.Component {
     return <div>
       {this.props.children}
     </div>;
+  }
+}
+
+@Radium
+export class MessageLayout extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+@Radium
+export class MessagePartLayout extends React.Component {
+  render() {
+    return (
+      <div style={this.severity(this.props.severity)}>
+        {this.props.message}
+      </div>
+    );
+  }
+
+  severity(str) {
+    const selector = {
+      Green: styles.green,
+      Orange: styles.orange,
+      Red: styles.red,
+    }
   }
 }
