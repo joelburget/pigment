4c66857245b2a6ff26106e7709a39d88397b1fa0
Joel Burget <joelburget@gmail.com>
2015-07-22 15:07:12 +0800

    Add note on how I imagine the data interaction working.

--- src-web/js/view/Data.js
+++ src-web/js/view/Data.js
@@ -62,6 +62,27 @@ export class DataConstructorLayout extends React.Component {
 }
 
 
+// TODO:
+// * button to add (/remove) a type parameter
+//   - this should allow them to be annotated with their kind
+//     (or inferred as Set) if it's left off?
+// * button to add (/remove) a constructor (or modifying?)
+//   - this should autocomplete from the type parameters and types in scope:
+//
+//       a_
+//     -----------------------
+//     | a      type parameter
+//     | arrow  in scope
+//     | align  in scope
+//     | ...
+//
+// * A big thing to think about is how breaking changes are handled. Sure,
+// we're fine if this thing isn't yet used. Punting on this for now, but I picture it opening a mode where you can resolve all the issues. Would be nice if you could bundle all changes to a data type, for instance:
+//   - "unlock"
+//   - make your changes - adding and removing params / ctors
+//   - "finish"
+//   - resolve (I imagine something like
+//     http://unisonweb.org/2015-06-12/editing.html#post-start)
 export class DataLayout extends React.Component {
   render() {
     // TODO replace by term
