7917aaca31578d6c09963b4d872c05412da2fdd7
Joel Burget <joelburget@gmail.com>
2015-10-06 16:37:02 -0400

    Import List (which label uses).

--- src/aspects/label/data.js
+++ src/aspects/label/data.js
@@ -1,7 +1,7 @@
 // @flow
 
 import invariant from 'invariant';
-import { Record } from 'immutable';
+import { List, Record } from 'immutable';
 
 import { mkSuccess } from '../../theory/evaluation';
 import { register } from '../../theory/registry';
