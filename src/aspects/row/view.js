7a765d6ed76e5ecf8b6aa62c9e5655e840121560
Joel Burget <joelburget@gmail.com>
2015-10-07 11:35:24 -0400

    Improve the row display.

--- src/aspects/row/view.js
+++ src/aspects/row/view.js
@@ -11,17 +11,26 @@ export default class Row extends Component {
       .entrySeq()
       .toArray()
       .map(([name, value]) => {
+        // TODO I think you should really be able to click on any of these rows
+        // -- right now you can only click on the expression.
+        //
+        // Maybe it doesn't make sense to select the row, since it's not really
+        // an expression, but you need to be able to rename and remove items,
+        // at least.
         return (
-          <Expression path={this.props.path.push(name)}>
-            {value}
-          </Expression>
+          <div className={style.rowRow}>
+            {name} :
+            <Expression path={this.props.path.push('entries').push(name)}>
+              {value}
+            </Expression>
+          </div>
         );
       });
 
     return (
       <div className={style.row}>
-        <div>{'{'}</div>
-        <div>
+        <div>Row {'{'}</div>
+        <div className={style.rowList}>
           {children}
         </div>
         <div>{'}'}</div>
