9784cbe527d3237b42cf0ceec1e248b29b51c2c4
Joel Burget <joelburget@gmail.com>
2015-10-10 10:11:12 -0400

    Add relation file.

---
+++ src/theory/relation.js
@@ -0,0 +1,26 @@
+// @flow
+
+// The notion of a relation between two terms (by which I mean terms, types,
+// kinds, etc).
+//
+// * x "is of type" y
+// * x "accepts term" y
+//   - This is really the same as the first: y "is of type" x
+//   - Is it though? The relationship is directional, only telling us about x.
+// * x "is" y
+//   - perhaps this is subtype-aware?
+//
+// I'm not yet sure if these are all the relations we care about, but they're
+// all I can think of at the moment.
+
+import { Relation } from 'immutable';
+
+export const IS_TYPE = 'IS_TYPE';
+export const ACCEPTS_TERM = 'ACCEPTS_TERM';
+export const MATCHES = 'MATCHES';
+
+export const Relation = Record({
+  type: MATCHES, // x _ y
+  subject: null, // _ matches y
+  object: null,  // x matches _
+});
