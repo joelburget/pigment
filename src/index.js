53264e17a03d8e45750c3610e044068ecb673634
Joel Burget <joelburget@gmail.com>
2015-11-12 10:44:51 -0500

    Remove GadgetPage, containers dir.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { PropTypes, Component } from 'react';
 import ReactDOM from 'react-dom';
-import GadgetPage from './containers/GadgetPage';
+
+import { Module, ModuleTy, ModuleView, ModuleTyView } from './models/Module';
+import { Location } from './models/Location';
+import Firmament, { fromBaseLocation } from './models/Firmament';
 
 
-ReactDOM.render(
-  <GadgetPage />,
-  document.getElementById('root')
-);
+const initialBaseLocation = new Location({
+  implementation: new Module(),
+  implementationView: ModuleView,
+  type: new ModuleTy(),
+  typeView: ModuleTyView,
+});
+
+
+export default class Page extends Component {
+
+  static childContextTypes = {
+    update: PropTypes.func,
+    signal: PropTypes.func,
+    global: PropTypes.instanceOf(Firmament),
+  };
+
+  state = {
+    global: fromBaseLocation(initialBaseLocation),
+  };
+
+  getChildContext() {
+    return {
+      signal: (path, action) => this.signal(path, action),
+      update: global => this.setState({ global }),
+      global: this.state.global,
+    };
+  }
+
+  signal(path, action) {
+    const { global } = this.state;
+    const location = global.get(path);
+    const newGlobal = location.signal(global, action);
+    this.setState({ global: newGlobal });
+  }
+
+  render() {
+    return (
+      <div style={typedTermStyle}>
+        <ModuleView path={[]} level={0} />
+        <ModuleTyView path={[]} level={1} />
+      </div>
+    );
+  }
+}
+
+const typedTermStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+};
+
+
+ReactDOM.render(<Page />, document.getElementById('root'));
